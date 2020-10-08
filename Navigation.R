5120436b25dbaba74e948bde1dfdddf1a4481b16
javiargos <jargosg@gmail.com>
2020-10-08 11:33:27 +0200

    upload navigation

---
+++ Navigation.R
@@ -0,0 +1,37 @@
+#21931310 JAVIER ARGOS GONZALEZ ####
+#Title: Navigating RStudio UI (user interface)####
+#File: Navigating throught R UI
+#Project: R_EssT_1; R Essential Training, Part 1:
+#         Wrangling and visualizing Data
+
+
+
+#INSTALL AND LOAD PACKAGES####
+library(datasets) #importing datasets
+
+#LOAD AND PREPARE DATA####
+?iris #help about iris dataset
+df <- iris #create var called df and assign iris dataset
+head(df) #print first elements 
+df
+
+#ANALYNE DATA#####
+hist(df$Petal.Width,
+     main = "European university - Histogram of petal Width",
+     xlab = "Petal Width (in cm)")
+
+#EXERCISE (Petal) and (Species)
+
+# CLEAN UP ####
+
+#clear enviroment
+rm(list = ls())
+
+#clear packages
+detach("package:datasets", unload = TRUE) #for base
+
+#clear plots
+graphics.off() #clear plots, closas all graphics devices
+
+#clear console
+cat("\014") #ctrl+L
